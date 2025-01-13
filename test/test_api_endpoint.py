c86463f00fa26d72d376445176c7bdd5f24092bf
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2025-01-13 14:52:35 +0530

    Update test_api_endpoint.py

--- test/test_api_endpoint.py
+++ test/test_api_endpoint.py
@@ -4,8 +4,8 @@ response = requests.post(
     "http://localhost:8000/api/analyze-image-url",
     json={
         "image_url": "https://t3.ftcdn.net/v2/jpg/01/76/69/98/1000_F_176699844_adobeanalytics.jpg",
-        "api_key1": "AIzaSyAiO3ZAvwwo7bmXsSOjWlYJuhkMCtzFCWw",
-        "api_key2": "AIzaSyAQxZOE2yQOS5fl8ehT-YXkbOaX-vcYu9I"
+        "api_key1": "api",
+        "api_key2": "-api"
     }
 )
 
